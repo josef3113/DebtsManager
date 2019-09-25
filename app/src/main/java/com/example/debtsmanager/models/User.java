66fe567dafc796b845049beae8ee48f0196d6ea4
josef3113 <33261454+josef3113@users.noreply.github.com>
2019-09-25 11:07:09 +0300

    add models user and debt

---
+++ app/src/main/java/com/example/debtsmanager/models/User.java
@@ -0,0 +1,33 @@
+package com.example.debtsmanager.models;
+
+public class User {
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    private String Email;
+    private String Name;
+    private String Password;
+
+
+}
